fea0a0354679693a927dbfbafa7b37204180f7c5
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-08 17:55:26 +0600

    Create category entity and update create-category-input

--- src/categories/dto/create-category.input.ts
+++ src/categories/dto/create-category.input.ts
@@ -2,6 +2,12 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateCategoryInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'title for category' })
+  title: string;
+
+  @Field({ nullable: true })
+  photo: string;
+
+  @Field(() => Int, { nullable: true })
+  parentId: number;
 }
--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,7 +1,25 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Category {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  title: string;
+
+  @Column()
+  @Field()
+  photo?: string;
+
+  @Column({ default: null })
+  @Field((type) => Int)
+  parentId?: number;
+
+  // @Field(() => Int, { description: 'Example field (placeholder)' })
+  // exampleField: number;
 }
