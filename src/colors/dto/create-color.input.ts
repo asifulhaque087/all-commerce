a61bf87594e0fe33cc77830144427ad09a3fe349
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:07:10 +0600

    Fix color dtos

--- src/colors/dto/create-color.input.ts
+++ src/colors/dto/create-color.input.ts
@@ -2,6 +2,6 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateColorInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'name for color' })
+  name: string;
 }
--- src/colors/dto/update-color.input.ts
+++ src/colors/dto/update-color.input.ts
@@ -5,4 +5,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 export class UpdateColorInput extends PartialType(CreateColorInput) {
   @Field(() => Int)
   id: number;
+
+  @Field(() => String, { nullable: true })
+  name: string;
 }
