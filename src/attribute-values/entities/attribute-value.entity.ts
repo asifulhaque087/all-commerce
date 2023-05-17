82ab053e5899255a83879394514f8121cf1f33c8
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:00:48 +0600

    Update attribute value entity

--- src/attribute-values/entities/attribute-value.entity.ts
+++ src/attribute-values/entities/attribute-value.entity.ts
@@ -1,7 +1,35 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Attribute } from 'src/attributes/entities/attribute.entity';
+import { ProductAttributeValue } from 'src/products/entities/product.entity';
+import { VariationValue } from 'src/variations/entities/variation.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class AttributeValue {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany((type) => ProductAttributeValue, (pattval) => pattval.value)
+  @Field((type) => [ProductAttributeValue])
+  pattvals: ProductAttributeValue[];
+
+  @ManyToOne((type) => Attribute, (variation) => variation.values)
+  @Field((type) => Attribute)
+  attribute: Attribute;
+
+  @OneToMany((type) => VariationValue, (varval) => varval.variation)
+  @Field((type) => [VariationValue])
+  varvals: VariationValue[];
 }
