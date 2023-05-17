352fd34c1534602f5fc62776d8acd33d1ffde5ff
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:03:07 +0600

    Update attribute entity

--- src/attributes/entities/attribute.entity.ts
+++ src/attributes/entities/attribute.entity.ts
@@ -1,7 +1,24 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { AttributeValue } from 'src/attribute-values/entities/attribute-value.entity';
+import { ProductAttributeValue } from 'src/products/entities/product.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Attribute {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany((type) => ProductAttributeValue, (pattval) => pattval.attribute)
+  @Field((type) => [ProductAttributeValue])
+  pattvals: ProductAttributeValue[];
+
+  @OneToMany((type) => AttributeValue, (attval) => attval.attribute)
+  @Field((type) => [AttributeValue])
+  values: AttributeValue[];
 }
