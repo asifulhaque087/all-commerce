de139dc714c1049069ed0380e6f4e3900012ba77
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:02:44 +0600

    Import all necessary dependencies

--- src/attribute-values/attribute-values.module.ts
+++ src/attribute-values/attribute-values.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AttributeValuesService } from './attribute-values.service';
 import { AttributeValuesResolver } from './attribute-values.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AttributeValue } from './entities/attribute-value.entity';
+import { AttributesModule } from 'src/attributes/attributes.module';
 
 @Module({
-  providers: [AttributeValuesResolver, AttributeValuesService]
+  imports: [TypeOrmModule.forFeature([AttributeValue]), AttributesModule],
+  providers: [AttributeValuesResolver, AttributeValuesService],
+  exports: [AttributeValuesService],
 })
 export class AttributeValuesModule {}
