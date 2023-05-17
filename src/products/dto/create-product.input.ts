75291c37644d084797c2abbc132aad83cc220e1f
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:10:07 +0600

    Refactor product dtos

--- src/products/dto/create-product.input.ts
+++ src/products/dto/create-product.input.ts
@@ -1,12 +1,12 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
-class OptionsWithVaritions {
+class ValuesWithAttributes {
   @Field(() => Int)
-  optionId: number;
+  valueId: number;
 
   @Field(() => Int)
-  variationId: number;
+  attributeId: number;
 }
 
 @InputType()
@@ -14,50 +14,20 @@ export class CreateProductInput {
   @Field(() => String, { description: 'name for product' })
   name: string;
 
-  // @Field(() => String, { description: 'description for product' })
-  // description: string;
-
   @Field(() => [ColorsWithImgs], { nullable: true })
   colorsWithImages: ColorsWithImgs[];
 
-  @Field(() => [OptionsWithVaritions], { nullable: true })
-  optionsWithVariations: OptionsWithVaritions[];
-
-  // @Field(() => Int, { nullable: true })
-  // variationId: number;
-
-  // @Field(() => Int, { nullable: true })
-  // optionId: number;
+  @Field(() => [ValuesWithAttributes], { nullable: true })
+  valuesWithAttributes: ValuesWithAttributes[];
 }
 
 @InputType()
-export class AddOptionToProduct {
+export class AddValueToProduct {
   @Field(() => Int)
   productId: number;
 
-  @Field(() => [OptionsWithVaritions])
-  optionsWithVariations: OptionsWithVaritions[];
-}
-
-@InputType()
-export class CreateVariationInput {
-  @Field(() => String, { description: 'name for variation' })
-  name: string;
-}
-
-@InputType()
-export class CreateOptionInput {
-  @Field(() => String, { description: 'name for option' })
-  name: string;
-
-  @Field(() => Int, { nullable: true })
-  variation: number;
-}
-
-@InputType()
-export class CreateColorInput {
-  @Field(() => String, { description: 'name for color' })
-  name: string;
+  @Field(() => [ValuesWithAttributes])
+  valuesWithAttributes: ValuesWithAttributes[];
 }
 
 @InputType()
@@ -76,31 +46,4 @@ export class AddColorToProduct {
 
   @Field(() => [ColorsWithImgs])
   colorsWithImages: ColorsWithImgs[];
-
-  // @Field(() => [Int])
-  // colorId: number[];
-
-  // @Field(() => [String])
-  // img: string[];
-}
-
-@InputType()
-export class CreateCombinationInput {
-  @Field(() => Int)
-  productId: number;
-
-  @Field(() => String)
-  color: string;
-
-  @Field(() => String)
-  img: string;
-
-  @Field(() => Int)
-  stock: number;
-
-  @Field(() => Int)
-  price: number;
-
-  @Field(() => [Int])
-  options: number[];
 }
--- src/products/dto/update-product.input.ts
+++ src/products/dto/update-product.input.ts
@@ -1,10 +1,4 @@
-import {
-  CreateColorInput,
-  CreateCombinationInput,
-  CreateOptionInput,
-  CreateProductInput,
-  CreateVariationInput,
-} from './create-product.input';
+import { CreateProductInput } from './create-product.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
@@ -20,54 +14,4 @@ export class UpdateProductInput extends PartialType(CreateProductInput) {
 
   @Field(() => Int, { nullable: true })
   option: number;
-}
-
-@InputType()
-export class UpdateVariationInput extends PartialType(CreateVariationInput) {
-  @Field(() => Int)
-  id: number;
-
-  @Field(() => String, { nullable: true })
-  name: string;
-}
-
-@InputType()
-export class UpdateOptionInput extends PartialType(CreateOptionInput) {
-  @Field(() => Int)
-  id: number;
-
-  @Field(() => String, { nullable: true })
-  name: string;
-
-  @Field(() => Int, { nullable: true })
-  variation: number;
-}
-
-@InputType()
-export class UpdateColorInput extends PartialType(CreateColorInput) {
-  @Field(() => Int)
-  id: number;
-
-  @Field(() => String, { nullable: true })
-  name: string;
-}
-
-@InputType()
-export class UpdateCombinationInput extends PartialType(
-  CreateCombinationInput,
-) {
-  @Field(() => Int)
-  id: number;
-
-  @Field(() => String, { nullable: true })
-  color: string;
-
-  @Field(() => String, { nullable: true })
-  img: string;
-
-  @Field(() => Int, { nullable: true })
-  stock: number;
-
-  @Field(() => Int, { nullable: true })
-  price: number;
 }
