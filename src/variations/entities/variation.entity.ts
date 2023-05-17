652366c758272178440d21729e2a3a74a6eb1323
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:13:10 +0600

    Update variation entity

--- src/variations/entities/variation.entity.ts
+++ src/variations/entities/variation.entity.ts
@@ -1,7 +1,59 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { AttributeValue } from 'src/attribute-values/entities/attribute-value.entity';
+import { Product } from 'src/products/entities/product.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Variation {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  color: string;
+
+  @Column()
+  @Field()
+  img: string;
+
+  @Column()
+  @Field((type) => Int)
+  stock: number;
+
+  @Column()
+  @Field((type) => Int)
+  price: number;
+
+  @ManyToOne(() => Product, (product) => product.variations)
+  @Field((type) => Product)
+  product: Product;
+
+  @OneToMany((type) => VariationValue, (varval) => varval.variation)
+  @Field((type) => [VariationValue])
+  varvals: VariationValue[];
+}
+
+// variation value
+@ObjectType()
+@Entity()
+export class VariationValue {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @ManyToOne(() => Variation, (varval) => varval.varvals)
+  @Field((type) => Variation)
+  variation: Variation;
+
+  @ManyToOne(() => AttributeValue, (attval) => attval.varvals)
+  @Field((type) => AttributeValue)
+  value: AttributeValue;
 }
