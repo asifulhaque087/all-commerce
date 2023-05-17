fb02ff2515c027e5becdc80b3219828c415810c0
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:06:34 +0600

    Upate color entity

--- src/colors/entities/color.entity.ts
+++ src/colors/entities/color.entity.ts
@@ -1,7 +1,19 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ProductColor } from 'src/products/entities/product.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Color {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany((type) => ProductColor, (pc) => pc.color)
+  @Field((type) => [ProductColor])
+  img: ProductColor[];
 }
