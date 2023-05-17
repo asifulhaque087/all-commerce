11c07abe56174f1beb9b321acaa0d0dd5caf8c9a
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:14:45 +0600

    Add variation service functionality

--- src/variations/variations.service.ts
+++ src/variations/variations.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVariationInput } from './dto/create-variation.input';
 import { UpdateVariationInput } from './dto/update-variation.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Variation, VariationValue } from './entities/variation.entity';
+import { Repository } from 'typeorm';
+import { ProductsService } from 'src/products/products.service';
+import { AttributesService } from 'src/attributes/attributes.service';
+import { AttributeValuesService } from 'src/attribute-values/attribute-values.service';
 
 @Injectable()
 export class VariationsService {
-  create(createVariationInput: CreateVariationInput) {
-    return 'This action adds a new variation';
+  constructor(
+    @InjectRepository(Variation) private variationModel: Repository<Variation>,
+    @InjectRepository(VariationValue)
+    private varvalModel: Repository<VariationValue>,
+    private ProductsService: ProductsService,
+    private AttributeValuesService: AttributeValuesService,
+  ) {}
+
+  async create(createVariationInput: CreateVariationInput) {
+    const { productId, color, img, stock, price, values } =
+      createVariationInput;
+
+    const variation = await this.variationModel.create({
+      color,
+      img,
+      stock,
+      price,
+    });
+
+    const product = await this.ProductsService.findOne(productId);
+
+    variation.product = product;
+
+    await this.variationModel.save(variation);
+
+    for (let i = 0; i < values.length; i++) {
+      const value = await this.AttributeValuesService.findOne(values[i]);
+      const cmbo = await this.varvalModel.create({
+        variation,
+        value,
+      });
+      await this.varvalModel.save(cmbo);
+    }
+    return variation;
   }
 
   findAll() {
@@ -13,14 +51,19 @@ export class VariationsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} variation`;
+    return this.variationModel.findOneBy({ id });
   }
 
-  update(id: number, updateVariationInput: UpdateVariationInput) {
-    return `This action updates a #${id} variation`;
+  async update(id: number, updateVariationInput: UpdateVariationInput) {
+    const variation = await this.findOne(id);
+    Object.assign(variation, updateVariationInput);
+    return this.variationModel.save(variation);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} variation`;
+  async remove(id: number) {
+    const variation = await this.findOne(id);
+    const varitionCopy = Object.assign({}, variation);
+    await this.variationModel.remove(variation);
+    return varitionCopy;
   }
 }
