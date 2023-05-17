27b482caf31b38018b3a187fb3242bb359df21af
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:15:22 +0600

    Update variation module

--- src/variations/variations.module.ts
+++ src/variations/variations.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { VariationsService } from './variations.service';
 import { VariationsResolver } from './variations.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Variation, VariationValue } from './entities/variation.entity';
+import { ProductsModule } from 'src/products/products.module';
+import { AttributeValuesModule } from 'src/attribute-values/attribute-values.module';
 
 @Module({
-  providers: [VariationsResolver, VariationsService]
+  imports: [
+    TypeOrmModule.forFeature([Variation, VariationValue]),
+    ProductsModule,
+    AttributeValuesModule,
+  ],
+  providers: [VariationsResolver, VariationsService],
 })
 export class VariationsModule {}
