e17dca21cc4986e6e79da666b6ad544f7fd89f25
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-18 00:03:35 +0600

    Update attribute dtos

--- src/attributes/dto/update-attribute.input.ts
+++ src/attributes/dto/update-attribute.input.ts
@@ -5,4 +5,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 export class UpdateAttributeInput extends PartialType(CreateAttributeInput) {
   @Field(() => Int)
   id: number;
+
+  @Field(() => String, { nullable: true })
+  name: string;
 }
