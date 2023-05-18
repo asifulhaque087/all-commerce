c499f5a8e38cf828e9b27f37f85ec8e3d94f87d5
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 01:56:30 +0600

    Fetch single permission

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePermissionInput } from './dto/create-permission.input';
 import { UpdatePermissionInput } from './dto/update-permission.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Permission } from './entities/permission.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PermissionsService {
+  constructor(
+    @InjectRepository(Permission)
+    private permissionModel: Repository<Permission>,
+  ) {}
+
   create(createPermissionInput: CreatePermissionInput) {
     return 'This action adds a new permission';
   }
@@ -13,7 +21,7 @@ export class PermissionsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} permission`;
+    return this.permissionModel.findOneBy({ id });
   }
 
   update(id: number, updatePermissionInput: UpdatePermissionInput) {
