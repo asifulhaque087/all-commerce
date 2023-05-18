bd2064f2f41dde8c8dcce24f59a7cdaadca3ff72
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 00:03:49 +0600

    Add permission field name

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,7 +1,23 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Role } from 'src/roles/entities/role.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Permission {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  action: string;
+
+  @Column()
+  @Field()
+  subject: string;
+
+  @ManyToOne((type) => Role, (role) => role.permissions)
+  @Field((type) => Role)
+  role: Role;
 }
