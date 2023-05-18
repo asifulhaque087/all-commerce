0259e61c1d66bfec343379c6b04b04552e5a009a
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 00:03:34 +0600

    Add role field name

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,7 +1,24 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Permission } from 'src/permissions/entities/permission.entity';
+import { UserRole } from 'src/users/entities/user.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Role {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany((type) => UserRole, (ur) => ur.role)
+  @Field((type) => [UserRole])
+  users: UserRole[];
+
+  @OneToMany((type) => Permission, (prm) => prm.role)
+  @Field((type) => [Permission])
+  permissions: Permission[];
 }
