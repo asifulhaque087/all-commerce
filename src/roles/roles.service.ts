14dc9b20ab1f70090b9d09a1ad133f0eef29995e
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 01:56:56 +0600

    Add method to assign permissions

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRoleInput } from './dto/create-role.input';
+import {
+  AssignPermissionsToRoleInput,
+  CreateRoleInput,
+} from './dto/create-role.input';
 import { UpdateRoleInput } from './dto/update-role.input';
 import { Role } from './entities/role.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PermissionsService } from 'src/permissions/permissions.service';
 
 @Injectable()
 export class RolesService {
-  constructor(@InjectRepository(Role) private roleModel: Repository<Role>) {}
+  constructor(
+    @InjectRepository(Role) private roleModel: Repository<Role>,
+    // services
+    private PermissionsService: PermissionsService,
+  ) {}
 
   async create(createRoleInput: CreateRoleInput) {
     const { name } = createRoleInput;
@@ -38,5 +46,32 @@ export class RolesService {
     const roleCopy = Object.assign({}, role);
     await this.roleModel.remove(role);
     return roleCopy;
+  }
+  // -- [[ crud end ]] --
+
+  async assignPermissionsToRole(
+    assignPermissionsToRoleInput: AssignPermissionsToRoleInput,
+  ) {
+    // destructure dto
+    const { roleId, permissionIds } = assignPermissionsToRoleInput;
+
+    // finding role
+    const role = await this.findOne(roleId);
+
+    for (let i = 0; i < permissionIds.length; i++) {
+      await this.assignPermissionToRoleUtil(role, permissionIds[i]);
+    }
+
+    return role;
+  }
+
+  async assignPermissionToRoleUtil(role: Role, permissionId: number) {
+    // finding coresponding instance
+    const permission = await this.PermissionsService.findOne(permissionId);
+    // assigning permission to role
+    role.permissions = [permission];
+    // saving the role
+    await this.roleModel.save(role);
+    return role;
   }
 }
