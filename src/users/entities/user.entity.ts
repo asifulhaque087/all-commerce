1e59dd9041ce49b2e9c69d160bb0b59bdd64c60f
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 00:03:20 +0600

    Add user field name

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,54 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Role } from 'src/roles/entities/role.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  email: string;
+
+  @Column()
+  @Field()
+  phone: string;
+
+  @Column()
+  @Field()
+  password: string;
+
+  @OneToMany((type) => UserRole, (ur) => ur.user)
+  @Field((type) => [UserRole])
+  roles: UserRole[];
+}
+
+// UserRole
+@ObjectType()
+@Entity()
+export class UserRole {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.roles)
+  @Field((type) => User)
+  user: User;
+
+  @ManyToOne(() => Role, (role) => role.users)
+  @Field((type) => Role)
+  role: Role;
 }
