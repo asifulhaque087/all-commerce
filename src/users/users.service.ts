3886eaa362646aba1f46f2fb68751909a496774f
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 01:10:54 +0600

    Add user crud operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AddRoleToUserInput, CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserRole } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userModel: Repository<User>,
+    @InjectRepository(UserRole) private urModel: Repository<UserRole>,
+  ) {}
+
+  async create(createUserInput: CreateUserInput) {
+    const { name, email, phone } = createUserInput;
+    let { password } = createUserInput;
+
+    // finding user
+    let user = await this.userModel.findOneBy({ email });
+
+    // if not user throw error
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    // hash password
+    password = await bcrypt.hash(password, 12);
+
+    // creating user instance
+    user = this.userModel.create({
+      name,
+      email,
+      password,
+      phone,
+    });
+
+    // saving user
+    this.userModel.save(user);
+
+    // generate token
+    const token = this.getSignedJwtToken(user);
+
+    return token;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userModel.findOneBy({ id });
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserInput: UpdateUserInput) {
+    const user = await this.findOne(id);
+    Object.assign(user, updateUserInput);
+    return this.userModel.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    const userCopy = Object.assign({}, user);
+    await this.userModel.remove(user);
+    return userCopy;
   }
+  // -- [[ crud end ]] --
+
+  // utils methods
+  getSignedJwtToken(user: User) {
+    return jwt.sign({ id: user.id }, 'MRIDUL', { expiresIn: '5h' });
+  }
+
+  // matchPassword(password: string) {
+  //   return bcrypt.compare(password, this.password);
+  // }
 }
