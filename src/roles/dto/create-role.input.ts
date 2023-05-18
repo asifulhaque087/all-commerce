5fbbdc5a23b68d2de0f565ecdd1ab49d249b7991
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 01:54:47 +0600

    Add dto for assigning permissions to role

--- src/roles/dto/create-role.input.ts
+++ src/roles/dto/create-role.input.ts
@@ -5,3 +5,12 @@ export class CreateRoleInput {
   @Field(() => String, { description: 'name for role' })
   name: string;
 }
+
+@InputType()
+export class AssignPermissionsToRoleInput {
+  @Field(() => Int)
+  roleId: number;
+
+  @Field(() => [Int])
+  permissionIds: number[];
+}
