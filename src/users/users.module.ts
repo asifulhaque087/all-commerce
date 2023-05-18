8a13f888af89bde771d0c6a5e92248dd5470466c
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-19 01:23:22 +0600

    Import roles service on user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User, UserRole } from './entities/user.entity';
+import { RolesModule } from 'src/roles/roles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserRole])],
+  imports: [TypeOrmModule.forFeature([User, UserRole]), RolesModule],
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
