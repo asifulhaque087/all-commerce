ae190aa33b9bb9a36a56640a489512603c266eef
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-20 18:15:47 +0600

    Update order create dto

--- src/order/dto/create-order.input.ts
+++ src/order/dto/create-order.input.ts
@@ -2,6 +2,27 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateOrderInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'price for products' })
+  itemsPrice: number;
+
+  @Field(() => Int)
+  taxPrice: number;
+
+  @Field(() => Int)
+  totalPrice: number;
+
+  @Field(() => Int)
+  shippingPrice: number;
+
+  @Field(() => String, { description: 'address for shipping' })
+  shippingAddress: string;
+
+  @Field(() => String, { description: 'number for shipping' })
+  shippingNumber: string;
+
+  @Field(() => String, { description: 'payment method name for order' })
+  paymentMethod: string;
+
+  @Field(() => [Int])
+  orderItems: number[];
 }
